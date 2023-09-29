3a785406969a27ed581b986447ece6c2590107ca
Mike Domingues <mikeicd@gmail.com>
2023-09-29 12:45:33 -0300

    Correções no código e novo experimento

--- experimento.py
+++ experimento.py
@@ -14,7 +14,7 @@ logging.basicConfig()
 
 current_directory = os.getcwd()
 
-wait_time = 5
+wait_time = 120
 
 
 def start_project(args):
@@ -58,27 +58,32 @@ def start_ads(args):
     bw = "1000000000"
     alg = ["cubic", "reno"] if args.proto is None else [args.proto]
     BER = ["100000", "1000000"] if args.ber is None else [args.ber]
-    e2e_delay = ["10000", "100000"] if args.delay is None else [args.delay]
+    e2e_delay = ["10000", "20000"] if args.delay is None else [args.delay]
     trafego_bg = ["500m", "750m"] if args.trafego is None else [args.trafego]
-    
+
     logging.warning(alg)
     logging.warning(BER)
     logging.warning(e2e_delay)
     logging.warning(trafego_bg)
-    
 
     headers = "rep,trafego,proto,ber,delay,time,addrsrc,portsrc,addrdest,portdest,nsei,nsei,transfbits,bps"
-    headers_command_sv = f"echo '{headers}' >> data/server.csv"
     headers_command_cl = f"echo '{headers}' >> data/cliente.csv"
-    sub = subprocess.Popen(
-        headers_command_sv,
+
+    subprocess.Popen(
+        headers_command_cl,
         shell=True,
         stdout=subprocess.PIPE,
         text=True,
     )
 
-    sub = subprocess.Popen(
-        headers_command_cl,
+    cmd_iperf_server_udp = f"sudo himage pc1@{id} iperf -s -u"
+
+    cmd_iperf_server = f"sudo himage pc2@{id} iperf -s"
+
+    subprocess.Popen(cmd_iperf_server_udp.split())
+
+    subprocess.Popen(
+        cmd_iperf_server,
         shell=True,
         stdout=subprocess.PIPE,
         text=True,
@@ -89,64 +94,41 @@ def start_ads(args):
             for proto in alg:
                 for ber in BER:
                     for e2e in e2e_delay:
+                        
                         ident = f"{rep},{trafego},{proto},{ber},{e2e}"
-
                         # Configuracao do link
                         cmd_link_router = f"sudo vlink -bw {bw} -BER {ber} -d {e2e} router1:router2@{id}"
 
                         # Configuracao trafego background
-                        cmd_iperf_server_udp = (
-                            f"sudo himage pc1@{id} iperf -s -u -t {wait_time+5}"
-                        )
-                        cmd_iperf_client_udp = f"sudo himage pc3@{id} iperf -c 10.0.0.20 -u -t {wait_time+3} -b {trafego}"
+                        cmd_iperf_client_udp = f"sudo himage pc3@{id} iperf -c 10.0.0.20 -u -t {wait_time} -b {trafego}"
 
                         # Configuracao testes dados
-                        ident_sv = f"echo -n '{ident},' >> data/server.csv"
                         ident_cl = f"echo -n '{ident},' >> data/cliente.csv"
-                        cmd_iperf_server = f"sudo himage pc2@{id} iperf -s -t {wait_time+1} -Z {proto} -e -y C >> data/server.csv"
                         cmd_iperf_client = f"sudo himage pc4@{id} iperf -c 10.0.1.20 -t {wait_time} -e -Z {proto} -y C >> data/cliente.csv"
 
                         ### Rodando comandos
 
                         # Run identificadores das linhas
-                        sub = subprocess.Popen(
-                            ident_sv,
-                            shell=True,
-                            stdout=subprocess.PIPE,
-                            text=True,
-                        )
-                        sub.wait()
-                        sub = subprocess.Popen(
+                        subprocess.Popen(
                             ident_cl,
                             shell=True,
                             stdout=subprocess.PIPE,
                             text=True,
-                        )
-                        sub.wait()
+                        ).wait()
 
                         # Run Link
-                        sub = subprocess.Popen(cmd_link_router.split())
-                        sub.wait()
+                        subprocess.Popen(cmd_link_router.split()).wait()
 
                         # Run trafego UDP
-                        sub = subprocess.Popen(cmd_iperf_server_udp.split())
                         subprocess.Popen(cmd_iperf_client_udp.split())
 
                         # Run trafego TCP
-                        subprocess.Popen(
-                            cmd_iperf_server,
-                            shell=True,
-                            stdout=subprocess.PIPE,
-                            text=True,
-                        )
                         subprocess.Popen(
                             cmd_iperf_client,
                             shell=True,
                             stdout=subprocess.PIPE,
                             text=True,
-                        )
-
-                        sub.wait()
+                        ).wait()
 
 
 if __name__ == "__main__":
@@ -160,9 +142,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-proto", type=str, default=None, help="Protocolo [cubic,reno]."
     )
-    parser.add_argument(
-        "-clean", type=bool, default=False, help="Clean files"
-    )
+    parser.add_argument("-clean", type=bool, default=False, help="Clean files")
 
     args = parser.parse_args()
 
--- processa_dados.py
+++ processa_dados.py
@@ -11,6 +11,10 @@ logging.basicConfig(
 )
 
 
+def plot_bar(x, mean, margin):
+    plt.bar(x, mean, yerr=margin, align="center", alpha=0.5, ecolor="black", capsize=10)
+
+
 def plot_confidence_interval(x, mean, margin_error, filtro, color="#2187bb", hlw=0.10):
     left = x - hlw
     top = mean - margin_error
@@ -20,30 +24,6 @@ def plot_confidence_interval(x, mean, margin_error, filtro, color="#2187bb", hlw
     plt.plot([top, top], [left, right], color=color)
     plt.plot([bottom, bottom], [left, right], color=color)
     plt.plot(mean, x, "o", color="#f44336")
-
-
-# def plot_c_interval(df, mean, margin_error, filtro, col):
-#     logging.debug(df)
-#     def plot_confidence_interval(x, mean, margin_error, color="#2187bb", hlw=0.10):
-#         left = x - hlw
-#         top = mean - margin_error
-#         right = x + hlw
-#         bottom = mean + margin_error
-#         plt.plot([bottom, top], [x, x], color=color)
-#         plt.plot([top, top], [left, right], color=color)
-#         plt.plot([bottom, bottom], [left, right], color=color)
-#         plt.plot(mean, x, "o", color="#f44336")
-#     lista = df.index.tolist()
-#     lista = [item + 1 for item in lista]
-#     plt.yticks(lista)
-#     plt.title(f"Confidence Interval {col} - {filtro}")
-#     plt.plot([mean, mean], [0, 11], color='#5555')
-#     for index, row in df.iterrows():
-#         x_value = index + 1
-#         mean_value = row[col]
-#         plot_confidence_interval(x_value, mean_value, margin_error)
-#     plt.savefig(f'data/plots/{col}-{filtro}.png')
-#     plt.close()
 
 
 def filter_df(df, **kwargs):
@@ -75,37 +55,40 @@ def analize_data(df):
     result_data = []
     columns = ["bps", "transfbits"]
     for col in columns:
-        i = 0
-        filters = []
-        plt.figure(figsize=(16,9))
-        for ut in df["trafego"].unique():
-            for up in df["proto"].unique():
-                for ub in df["ber"].unique():
-                    for ud in df["delay"].unique():
+        for ud in df["delay"].unique():
+            for ut in df["trafego"].unique():
+                i = 0
+                plt.figure(figsize=(16, 9))
+                filters = []
+                for up in df["proto"].unique():
+                    for ub in df["ber"].unique():
                         dff = filter_df(df, trafego=ut, proto=up, ber=ub, delay=ud)
                         dff = dff.reset_index(drop=True)
-                        filtro = f"{ut}{up}b{ub}d{ud}"
+                        filtro = f"{up}"
                         filters.append(filtro)
-                        
                         mean = dff[col].mean()
                         std = dff[col].std()
-                        margin_e = calculate_confidence_interval(
+                        margin = calculate_confidence_interval(
                             dff[col].size, mean, std, 0.99
                         )
 
-                        plot_confidence_interval(i + 1, mean, margin_e, filtro)
+                        # plot_confidence_interval(i + 1, mean, margin, filtro)
+                        plot_bar(i + 1, mean, margin)
                         i += 1
 
-                        result_data.append((filtro, mean/1000, std/1000, margin_e/1000, col))
-        
-        plt.yticks(np.arange(1, 17), filters)
-        plt.title(f"Confidence Interval {col}")
-        plt.savefig(f"data/plots/{col}-.png")
-        plt.close()
+                        result_data.append(
+                            (filtro, mean / 10000, std / 1000, margin / 1000, col)
+                        )
+
+                # plt.yticks(np.arange(1, len(filters) + 1), filters)
+                plt.xticks([1, 2], filters)
+                plt.title(f"Intervalo de confiança - {col} - {ud/1000}ns - {ut}")
+                plt.savefig(f"data/plots/{col}-{ud/1000}-{ut}.png")
+                plt.close()
 
     return pd.DataFrame(
         result_data,
-        columns=["filtro", "mean(M)", "std(M)", "margin_e(M)", "col"],
+        columns=["filtro", "mean(M)", "std(K)", "margin_e(K)", "col"],
     )
 
 
